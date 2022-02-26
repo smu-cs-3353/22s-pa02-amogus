b73df81693102656ba4a7b49219815a463f50111
gmongaras <gabriel@mongaras.com>
2022-02-26 01:43:32 -0600

    Implemented Intro Sort

--- src/introSort.cpp
+++ src/introSort.cpp
@@ -4,5 +4,23 @@
 
 template <typename itemtype>
 itemtype*& introSort(itemtype*& arr, int size) {
+    // Get the depth limit
+    int depthLimit = 2 * floor(log2(size));
+
+    // If the size of the array is less than 16, use insertionSort
+    if (size <= 16) {
+        insertionSort(arr, size);
+    }
+
+    // If the depth limit is 0, heap sort the array
+    else if (depthLimit == 0) {
+        heapSort(arr, size);
+    }
+
+    // If none of the conditions are met, quick sort the array
+    else {
+        randomizedQuickSort(arr, size);
+    }
+
     return arr;
 }
