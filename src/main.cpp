17a3d4d98e93d433c09e6072f46acce14db58727
gmongaras <gabriel@mongaras.com>
2022-02-26 01:33:43 -0600

    Implemented Heap Sort

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,15 @@
 #include "shellSort.h"
 #include "introSort.h"
 #include "timSort.h"
+#include "heapSort.cpp"
 #include "DrFontenotsFavoriteSorter.h"
 
 
 
 int main(int argc, char* argv[]) {
-    int* arr = new int[4]{4, 7, 0, 2};
-    arr = mergeSort(arr, 4);
-    std::cout << arr[0] << arr[1] << arr[2] << arr[3] << std::endl;
+    int* arr = new int[5]{4, 10, 3, 5, 1};
+    arr = heapSort(arr, 5);
+    std::cout << arr[0] << arr[1] << arr[2] << arr[3] << arr[4] << std::endl;
 
     return 0;
 }
