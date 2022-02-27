2a683973e53dc209827b7ab7eed125a9390af923
TrevorDohm <tdohm@smu.edu>
2022-02-27 16:29:48 -0600

    Completed ShellSort.

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "insertionSort.cpp"
 #include "randomizedQuickSort.cpp"
 #include "mergeSort.cpp"
-#include "shellSort.h"
+#include "shellSort.cpp"
 #include "introSort.h"
 #include "timSort.h"
 #include "heapSort.cpp"
@@ -12,7 +12,7 @@
 
 int main(int argc, char* argv[]) {
     int* arr = new int[5]{3, 7, 1, 9, 8};
-    arr = randomizedQuickSort(arr, 5);
+    arr = shellSort(arr, 5);
     std::cout << arr[0] << arr[1] << arr[2] << arr[3] << arr[4] << std::endl;
 
     return 0;
--- src/shellSort.h
+++ src/shellSort.h
@@ -1,9 +1,7 @@
 #ifndef INC_22S_PA02_SUSSYBAKA_SHELLSORT_H
 #define INC_22S_PA02_SUSSYBAKA_SHELLSORT_H
 
-
 template <typename itemtype>
-itemtype*& shellSort(itemtype*& arr, int size);
-
+itemtype* shellSort(itemtype* arr, int size);
 
 #endif //INC_22S_PA02_SUSSYBAKA_SHELLSORT_H
