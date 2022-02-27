f7da9dd35c8b0a4f9d3a448489496a63fc14117e
TrevorDohm <tdohm@smu.edu>
2022-02-27 15:11:38 -0600

    Working on QuickSort, not done (Added code but incorrect)

--- src/randomizedQuickSort.cpp
+++ src/randomizedQuickSort.cpp
@@ -1,8 +1,55 @@
 #include "randomizedQuickSort.h"
 
+// All elements smaller than pivot on left, all
+// elements larger than pivot on right, return
+template <typename itemtype>
+int partition(itemtype*& arr, int low, int high) {
 
+    // Random number between low and high (Pivot Gen)
+    srand(time(NULL));
+    int randPiv = rand() % (high - low);
 
+    // Incrementer i
+    int i = (low - 1);
+
+    // Incrementer j, complete partition
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < randPiv) {
+            i++;
+            std::swap(arr[i], arr[j]);
+        }
+    }
+
+    // Note i + 1 is Pivot Location
+    std::swap(arr[i + 1], arr[high]);
+    return (i + 1);
+
+}
+
+// Main function, called recursively with partitions
+template <typename itemtype>
+void quickSort(itemtype*& arr, int low, int high) {
+
+    // Else, base case
+    if (low < high) {
+
+        // Partition Index
+        int pi = partition(arr, low, high);
+
+        // Quicksort on elements greater than pivot
+        // and those less than pivot. Recursive
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+
+    }
+
+}
+
+// Simply calls Quicksort function and returns sorted array
 template <typename itemtype>
 itemtype*& randomizedQuickSort(itemtype*& arr, int size) {
+
+    quickSort(arr, 0, size - 1);
     return arr;
+
 }
--- src/randomizedQuickSort.h
+++ src/randomizedQuickSort.h
@@ -1,9 +1,12 @@
-#ifndef INC_22S_PA02_SUSSYBAKA_RANDOMIZEDQUICKSORT_H
-#define INC_22S_PA02_SUSSYBAKA_RANDOMIZEDQUICKSORT_H
+#ifndef INC_22S_PA02_AMOGUS_RANDOMIZEDQUICKSORT_H
+#define INC_22S_PA02_AMOGUS_RANDOMIZEDQUICKSORT_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <algorithm>
 
 template <typename itemtype>
 itemtype*& randomizedQuickSort(itemtype*& arr, int size);
 
-
-#endif //INC_22S_PA02_SUSSYBAKA_RANDOMIZEDQUICKSORT_H
+#endif //INC_22S_PA02_AMOGUS_RANDOMIZEDQUICKSORT_H
