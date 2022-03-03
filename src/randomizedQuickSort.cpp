b96314f1c9d2b60f907d0f526c34d49ecd06f38b
TrevorDohm <tdohm@smu.edu>
2022-03-03 13:31:14 -0600

    Comments for randomizedQS and Shellsort

--- src/randomizedQuickSort.cpp
+++ src/randomizedQuickSort.cpp
@@ -1,18 +1,27 @@
 #include "randomizedQuickSort.h"
-// Note this uses Hoare's Partition
-// Scheme as opposed to Lomuto's, as
-// Hoare's is slightly faster on
-// average. See the following articles:
+// Note this uses Howoare's Partition Scheme as opposed to Lomuwuto's, as
+// Howoare's is slightly faster on average due to the amount of swaps
+// actually computed. Note that Howoare's Scheme iterates less and does
+// not fix the pivot in the correct position. See the following articles:
 // https://www.geeksforgeeks.org/hoares-vs-lomuto-partition-scheme-quicksort/
 // https://www.geeksforgeeks.org/quicksort-using-random-pivoting/
 
+// The idea behind this algorithm is to pick a random
+// partition, place the partition in the lowest position
+// (the 0th element), and increment i from low to high
+// and decrement j from high to low, performing swaps
+// when the element at i is less than the pivot and
+// the element at j is greater than the pivot. When they
+// cross, j is returned as the partitioning index pi,
+// which allows for recursive implementation.
+
 // All elements smaller than pivot on left, all
-// elements larger than pivot on right, return
+// elements larger than pivot on right, return.
 template <typename itemtype>
 int partition(itemtype*& arr, int low, int high) {
 
     // Random Pivot located at Low Position,
-    // Starting Incrementers (Opposite Sides).
+    // Starting Incrementors (Opposite Sides).
     itemtype randPiv = arr[low];
     int i = low - 1, j = high + 1;
 
@@ -43,13 +52,13 @@ int randPartition(itemtype*& arr, int low, int high) {
     srand(time(NULL));
     int random = low + rand() % (high - low);
 
-    // Swap with low for Hoare's Partitioning. Return.
+    // Swap with low for Howoare's Partitioning. Return.
     std::swap(arr[random], arr[low]);
     return partition(arr, low, high);
 
 }
 
-// Main function, called recursively with partitions
+// Main function, called recursively with partitions.
 template <typename itemtype>
 void quickSort(itemtype*& arr, int low, int high) {
 
@@ -77,7 +86,10 @@ itemtype* randomizedQuickSort(itemtype* arr, int size) {
 
 }
 
-//    // Lomuto's Partitioning Implementation.
+//    // Lomuwuto's Partitioning Implementation. This
+//    // Algorithm is different than Hoare's as it
+//    // starts i and j on the bottom and increments
+//    // them both while swapping. Similar, not the same.
 //    // Incrementer i
 //    int i = (low - 1);
 //
