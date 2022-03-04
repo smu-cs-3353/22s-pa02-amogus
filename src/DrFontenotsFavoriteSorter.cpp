b3701d4e6782350280f6a2bed1358411b548be79
TrevorDohm <tdohm@smu.edu>
2022-03-03 18:50:14 -0600

    Header files and Sorter File comments.

--- src/DrFontenotsFavoriteSorter.cpp
+++ src/DrFontenotsFavoriteSorter.cpp
@@ -1,178 +1,180 @@
 #include "DrFontenotsFavoriteSorter.h"
 
-
-
-
-/************************
- **    Constructors    **
- ************************/
+// Constructor.
 DrFontenotsFavoriteSorter::DrFontenotsFavoriteSorter(std::vector<std::string> &fileNames, std::string& outfileName) {
     this->fileNames = fileNames;
     this->outfileName = outfileName;
 }
 
-
-
-/***************
- **    Run    **
- ***************/
+// Run Function.
 void DrFontenotsFavoriteSorter::run() {
-    // Open the output file
+
+    // Open the output file.
     std::ofstream outFile(outfileName);
 
-    // Add the header to the output file
+    // Add the header to the output file.
     outFile << "File Name,Size,Integer/String,Insertion,Random Quicksort,Merge,Shell,Intro,Tim" << std::endl;
 
-    // Iterate over all file names
+    // Iterate over all file names.
     for (std::string& name : fileNames) {
-        // Open the input file
+
+        // Open the input file.
         std::ifstream curFile(name);
 
-        // If the file is not open, raise an error
+        // If the file is not open, raise an error.
         if (!curFile.is_open()) {
             throw std::runtime_error("File " + name + " could not be opened.");
         }
 
-        // If the file is a string
+        // If the file is a string:
         if (name[7] == 's') {
-            // Holds the current line in the file
+
+            // Holds the current line in the file.
             std::string curLine;
 
-            // Get the size of the file
+            // Get the size of the file.
             int numLines = 0;
             while (std::getline(curFile, curLine)) {
                 ++numLines;
             }
 
-            // Reset the file pointer
+            // Reset the file pointer.
             curFile.clear();
             curFile.seekg(0);
 
-            // Create a string array which is the same size as the file
-            std::string* arr = new std::string[numLines];
+            // Create a string array which is the same size as the file.
+            auto* arr = new std::string[numLines];
 
             // Iterate over the file and add each line to the array.
             for (int i = 0; i < numLines; i++) {
-                // Get the next line
+
+                // Get the next line.
                 std::getline(curFile, curLine);
 
-                // Add the file to the file
+                // Add the file to the file.
                 arr[i] = curLine;
             }
 
-            // Output the beginning of the line to the file
+            // Output the beginning of the line to the file.
             outFile << name << "," << numLines << "," << "string" << ",";
 
-            // Insertion Sort
+            // Insertion Sort:
             auto start = std::chrono::high_resolution_clock::now();
             insertionSort(arr, numLines);
             auto stop = std::chrono::high_resolution_clock::now();
             outFile << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() << ",";
 
-            // Randomized QuickSort
+            // Randomized QuickSort:
             start = std::chrono::high_resolution_clock::now();
             randomizedQuickSort(arr, numLines);
             stop = std::chrono::high_resolution_clock::now();
             outFile << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() << ",";
 
-            // Merge Sort
+            // Merge Sort:
             start = std::chrono::high_resolution_clock::now();
             mergeSort(arr, numLines);
             stop = std::chrono::high_resolution_clock::now();
             outFile << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() << ",";
 
-            // Shell Sort
+            // Shellsort:
             start = std::chrono::high_resolution_clock::now();
             shellSort(arr, numLines);
             stop = std::chrono::high_resolution_clock::now();
             outFile << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() << ",";
 
-            // Intro Sort
+            // Introsort:
             start = std::chrono::high_resolution_clock::now();
             introSort(arr, numLines);
             stop = std::chrono::high_resolution_clock::now();
             outFile << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() << ",";
 
-            // Tim Sort
+            // Timsort:
             start = std::chrono::high_resolution_clock::now();
             timSort(arr, numLines);
             stop = std::chrono::high_resolution_clock::now();
             outFile << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
             outFile << std::endl;
+
         }
-        // If the file is an integer
+
+        // If the file is an integer:
         else {
-            // Holds the current line in the file
+
+            // Holds the current line in the file.
             std::string curLine;
 
-            // Get the size of the file
+            // Get the size of the file.
             int numLines = 0;
             while (std::getline(curFile, curLine)) {
                 ++numLines;
             }
 
-            // Reset the file pointer
+            // Reset the file pointer.
             curFile.clear();
             curFile.seekg(0);
 
-            // Create a string array which is the same size as the file
+            // Create a string array which is the same size as the file.
             int* arr = new int[numLines];
 
             // Iterate over the file and add each line to the array.
             for (int i = 0; i < numLines; i++) {
-                // Get the next line
+
+                // Get the next line.
                 std::getline(curFile, curLine);
 
-                // Add the file to the file
+                // Add the file to the file.
                 arr[i] = stoi(curLine);
+
             }
 
-            // Output the beginning of the line to the file
+            // Output the beginning of the line to the file.
             outFile << name << "," << numLines << "," << "integer" << ",";
 
-            // Insertion Sort
+            // Insertion Sort:
             auto start = std::chrono::high_resolution_clock::now();
             insertionSort(arr, numLines);
             auto stop = std::chrono::high_resolution_clock::now();
             outFile << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() << ",";
 
-            // Randomized QuickSort
+            // Randomized QuickSort:
             start = std::chrono::high_resolution_clock::now();
             randomizedQuickSort(arr, numLines);
             stop = std::chrono::high_resolution_clock::now();
             outFile << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() << ",";
 
-            // Merge Sort
+            // Merge Sort:
             start = std::chrono::high_resolution_clock::now();
             mergeSort(arr, numLines);
             stop = std::chrono::high_resolution_clock::now();
             outFile << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() << ",";
 
-            // Shell Sort
+            // Shellsort:
             start = std::chrono::high_resolution_clock::now();
             shellSort(arr, numLines);
             stop = std::chrono::high_resolution_clock::now();
             outFile << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() << ",";
 
-            // Intro Sort
+            // Introsort:
             start = std::chrono::high_resolution_clock::now();
             introSort(arr, numLines);
             stop = std::chrono::high_resolution_clock::now();
             outFile << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() << ",";
 
-            // Tim Sort
+            // Timsort:
             start = std::chrono::high_resolution_clock::now();
             timSort(arr, numLines);
             stop = std::chrono::high_resolution_clock::now();
             outFile << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
             outFile << std::endl;
+
         }
 
-        // Close the current file
+        // Close the current file.
         curFile.close();
+
     }
 
-
-    // Close the output file
+    // Close the output file.
     outFile.close();
+
 }
--- src/introSort.h
+++ src/introSort.h
@@ -1,12 +1,10 @@
 #ifndef INC_22S_PA02_AMOGUS_INTROSORT_H
 #define INC_22S_PA02_AMOGUS_INTROSORT_H
 
-
 #include "insertionSort.cpp"
 #include "heapSort.cpp"
 #include <cmath>
 #include <iostream>
-
 
 template <typename itemtype>
 int partitionIntro(itemtype*& arr, int size, itemtype pivot);
@@ -19,6 +17,5 @@ itemtype* sort(itemtype* arr, int size, int maxDepth);
 
 template <typename itemtype>
 itemtype* introSort(itemtype* arr, int size);
-
 
 #endif //INC_22S_PA02_AMOGUS_INTROSORT_H
